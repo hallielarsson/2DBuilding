dde9299cf9ee961c0240c4e0b3ec00f3e4962cb9
halstedlarsson <halsted.larsson@gmail.com>
2015-08-02 15:03:18 -0400

    Added gridding of nodes
    Added a simple way to make a grid of map nodes.

--- Assets/Scripts/Nodes/NodeManager.cs
+++ Assets/Scripts/Nodes/NodeManager.cs
@@ -6,8 +6,12 @@ public class NodeManager : MonoBehaviour {
 
 	public List<MapNodeScript> nodes = new List<MapNodeScript>();
 	public Dictionary<MapNodeScript, bool> nodeSet = new Dictionary<MapNodeScript, bool>();
+	public MapNodeScript topLeftNode;
+	public MapNodeScript bottomRightNode;
+
 	// Use this for initialization
 	void Start () {
+		MakeGrid (topLeftNode, bottomRightNode, 10, 10);
 		nodes.AddRange (GetComponentsInChildren<MapNodeScript> ());
 	}
 	
@@ -29,5 +33,33 @@ public class NodeManager : MonoBehaviour {
 			}
 		});
 		return outNode;
+	}
+
+	public void MakeGrid(MapNodeScript topLeftNode, MapNodeScript bottomRightNode, int cols, int rows) {
+		Transform parent = transform;
+		Vector3 topLeft = topLeftNode.transform.position;
+		Vector3 bottomRight = bottomRightNode.transform.position;
+		Vector3 delta = bottomRight - topLeft;
+		float deltaX = 0f;
+		float deltaY = 0f;
+
+		for (float i = 0; i < cols; i++) {
+			deltaX =  i/(cols - 1);
+			for(float j = 0; j < rows; j++) {
+				deltaY = j/(rows - 1);
+				Vector3 newPos = topLeft + new Vector3(delta.x * deltaX, delta.y * deltaY,delta.z * deltaX);
+				MapNodeScript nodeScript;
+				if(deltaX == 0 && deltaY == 0){ 
+					nodeScript = topLeftNode;
+				} else if (deltaX == 1 && deltaY == 1) {
+					nodeScript = bottomRightNode;
+				} else {
+					GameObject node = GameObject.Instantiate<GameObject>(topLeftNode.gameObject);
+					node.transform.position = newPos;
+					node.transform.SetParent(parent);
+					nodeScript = node.GetComponent<MapNodeScript>();
+				}
+			}
+		}
 	}
 }
